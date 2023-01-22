835915f2490944766f28a16652e3ba5b0ba263ed
DaniBoteva <danifeya@gmail.com>
2023-01-22 23:06:15 +0200

    Added unit tets for Average method

--- Summator.UnitTests/SummatorTests.cs
+++ Summator.UnitTests/SummatorTests.cs
@@ -5,8 +5,9 @@ namespace Summator.UnitTests
     public class SummatorTests
     {
         [Test]
-        public void Test_Summator_Three_Positive_Numbers() { 
-            var nums = new[] { 1, 2, 3 };
+        public void Test_Summator_Three_Positive_Numbers()
+        {
+            var nums = new int[] { 1, 2, 3 };
             var actual = Summator.Sum(nums);
             var expected = 6;
             Assert.AreEqual(expected, actual);
@@ -15,7 +16,7 @@ namespace Summator.UnitTests
         [Test]
         public void Test_Summator_Two_Negative_Numbers()
         {
-            var nums = new[] { -1, -80 };
+            var nums = new int[] { -1, -80 };
             var actual = Summator.Sum(nums);
             var expected = -81;
             Assert.AreEqual(expected, actual);
@@ -24,7 +25,7 @@ namespace Summator.UnitTests
         [Test]
         public void Test_Summator_Negative_And_Positive_Number()
         {
-            var nums = new[] { -1, 100 };
+            var nums = new int[] { -1, 100 };
             var actual = Summator.Sum(nums);
             var expected = 99;
             Assert.AreEqual(expected, actual);
@@ -33,7 +34,7 @@ namespace Summator.UnitTests
         [Test]
         public void Test_Summator_Zero_And_Positive_Number()
         {
-            var nums = new[] { 0, 100 };
+            var nums = new int[] { 0, 100 };
             var actual = Summator.Sum(nums);
             var expected = 100;
             Assert.AreEqual(expected, actual);
@@ -42,7 +43,7 @@ namespace Summator.UnitTests
         [Test]
         public void Test_Summator_Zero_And_Negative_Number()
         {
-            var nums = new[] { 0, -20 };
+            var nums = new int[] { 0, -20 };
             var actual = Summator.Sum(nums);
             var expected = -20;
             Assert.AreEqual(expected, actual);
@@ -51,7 +52,7 @@ namespace Summator.UnitTests
         [Test]
         public void Test_Summator_Zero_And_Zero_Number()
         {
-            var nums = new[] { 0, 0 };
+            var nums = new int[] { 0, 0 };
             var actual = Summator.Sum(nums);
             var expected = 0;
             Assert.AreEqual(expected, actual);
@@ -60,16 +61,16 @@ namespace Summator.UnitTests
         [Test]
         public void Test_Summator_MinusZero_And_Zero_Number()
         {
-            var nums = new[] { -0, 0 };
+            var nums = new int[] { -0, 0 };
             var actual = Summator.Sum(nums);
-            var expected = -0; 
+            var expected = -0;
             Assert.AreEqual(expected, actual);
         }
 
         [Test]
         public void Test_Summator_MinusZero_And_MinusZero_Number()
         {
-            var nums = new[] { -0, -0 };
+            var nums = new int[] { -0, -0 };
             var actual = Summator.Sum(nums);
             var expected = 0;
             Assert.AreEqual(expected, actual);
@@ -78,10 +79,78 @@ namespace Summator.UnitTests
         [Test]
         public void Test_Summator_One_Number()
         {
-            var nums = new[] { -5 };
+            var nums = new int[] { -5 };
             var actual = Summator.Sum(nums);
             var expected = -5;
             Assert.AreEqual(expected, actual);
         }
+
+        [Test]
+        public void Test_Summator_Zero_Numbers()
+        {
+            var nums = new int[] { };
+            var actual = Summator.Sum(nums);
+            var expected = 0;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Test_Summator_Big_Numbers()
+        {
+            var nums = new int[] {2000000000, 2000000000, 2000000000};
+            var actual = Summator.Sum(nums);
+            var expected = 6000000000;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Test_Average_Two_Positive_Numbers()
+        {
+            var nums = new double[] { 5, 7 };
+            var actual = Summator.Average(nums);
+            var expected = 6;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Test_Average_Positive_Negative_Numbers()
+        {
+            var nums = new double[] { 6, -12 };
+            var actual = Summator.Average(nums);
+            var expected = -3;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Test_Average_Two_Negative_Number()
+        {
+            var nums = new double[] { -6, -12 };
+            var actual = Summator.Average(nums);
+            var expected = -9;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Test_Average_Negative_Zero_Numbers()
+        {
+            var nums = new double[] { -6, 0 };
+            var actual = Summator.Average(nums);
+            var expected = -3;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Test_Average_Big_Numbers()
+        {
+            var nums = new double[] { 2000000000, 2000000000 };
+            var actual = Summator.Average(nums);
+            var expected = 2000000000;
+            Assert.AreEqual(expected, actual);
+        }
+
+
+
+
+
     }
 }
